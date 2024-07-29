7cc73ea99139eabc900409f89ca60c3cb8ff2e38
champagnekamal <akashkamal70@gmail.com>
2024-07-29 08:32:19 +0530

    url

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors());
 const server = http.createServer(app)
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:5173",
+        origin:"https://authbyakash.netlify.app/",
         methods:["GET","POST"],
     }
 })
